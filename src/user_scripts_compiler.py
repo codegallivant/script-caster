3abd72d62e21c7e796a245a71d47d2b512e9dec8
EpicThunderZ <janak3.1415et@gmail.com>
2022-08-07 23:37:57 +0530

    Removed redundancy

--- src/user_scripts_compiler.py
+++ src/user_scripts_compiler.py
@@ -5,23 +5,6 @@ from github import Github
 
 def update_scripts(access_token, username, repo_name, path):
 
-
-    # Erasing contents of folder
-
-    folder = f'{path}'
-    for filename in os.listdir(folder):
-        if filename == ".gitignore" or filename == "__init__.py":
-            continue
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-
     # Writing file contents from GitHub
 
     if access_token == None or access_token == "":
